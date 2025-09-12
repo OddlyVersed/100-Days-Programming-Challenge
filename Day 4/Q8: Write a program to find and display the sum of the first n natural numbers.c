5b7d8c040701ce3cd690204ab30ac717fc5b6ec8
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-09-13 01:39:39 +0530

    Update Q8: Write a program to find and display the sum of the first n natural numbers.c

--- Day 4/Q8: Write a program to find and display the sum of the first n natural numbers.c
+++ Day 4/Q8: Write a program to find and display the sum of the first n natural numbers.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int main()
+{
+	int i,sum=0,n;
+	printf("Enter a positive integer n = ");
+	scanf("%d",&n);
+	    if(n>0){
+	        for(i = 1;i<=n;++i){
+	            sum +=i;
+	        }
+	        printf("Sum=%d\n", sum);
+	    } else {
+	        printf("Please enter a positive integer.\n");
+	    }
+	
+    return 0;
+	
+}

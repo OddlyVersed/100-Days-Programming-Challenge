dc2d66d565336c0ce6dbca8392b42ab3316ab78e
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-09-13 01:15:19 +0530

    Update Q6: Write a program to swap two numbers using a third variable.c

--- Day 3/Q6: Write a program to swap two numbers using a third variable.c
+++ Day 3/Q6: Write a program to swap two numbers using a third variable.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+	int a,b,hold;
+	printf("Enter the numbers(a and b) you want to swap = ");
+	scanf("%d%d",&a,&b);
+	printf("before swap: %d%d\n",a,b);
+	hold=a;
+	a=b;
+	b=hold;
+	printf("after swap: %d%d",a,b);
+	
+    return 0;
+	
+}

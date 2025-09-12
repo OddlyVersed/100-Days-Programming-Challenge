5d0b77d427a3b0ae58bd016ca0fe7b90319599c6
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-09-13 00:15:51 +0530

    Update Q1: Write a program to input two numbers and display their sum.c

--- Day 1/Q1: Write a program to input two numbers and display their sum.c
+++ Day 1/Q1: Write a program to input two numbers and display their sum.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int a,b;
+    printf("Enter number a and b = ");
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    printf("the sum of a and b is = %d",c);
+
+    return 0;
+}

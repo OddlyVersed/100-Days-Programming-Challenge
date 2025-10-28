a73a584a04adf6c2ee8db2990747fa189faf5c8f
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-10-28 16:43:27 +0530

    Create Q35: Write a program to print all factors of a given number.c

---
+++ Day 18/Q35: Write a program to print all factors of a given number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}

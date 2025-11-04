71563a65333bc671060b0a39209a71c8f057e551
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-04 10:23:01 +0530

    Create Q60: Count positive, negative, and zero elements in an array.c

---
+++ Day 30/Q60: Count positive, negative, and zero elements in an array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i, pos = 0, neg = 0, zero = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            pos++;
+        } else if (arr[i] < 0) {
+            neg++;
+        } else {
+            zero++;
+        }
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, zero);
+
+    return 0;
+}

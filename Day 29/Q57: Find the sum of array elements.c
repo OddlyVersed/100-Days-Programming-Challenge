2153df7f2f90ed1308fe449f444a42bda14b08b9
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-04 10:18:21 +0530

    Add program to calculate sum of array elements

---
+++ Day 29/Q57: Find the sum of array elements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i, sum = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}

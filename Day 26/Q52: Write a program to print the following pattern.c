0b6b1a75a55b3885c3b856ca0bff2a95773ffdd4
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-04 10:05:49 +0530

    Add program to print asterisk pattern
    The program prints a specific pattern of asterisks, with varying numbers of asterisks on each line.

---
+++ Day 26/Q52: Write a program to print the following pattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int i;
+
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}

6c2b1653eb4006fa99ce8152c1e6152da73f9230
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-04 10:20:09 +0530

    Create Q58: Find the maximum and minimum element in an array.c

---
+++ Day 29/Q58: Find the maximum and minimum element in an array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i, max, min;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Max=%d, Min=%d\n", max, min);
+
+    return 0;
+}
+

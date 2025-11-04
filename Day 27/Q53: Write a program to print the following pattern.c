18435ddfe0aad2c64b69dd9dea94156c87eae49b
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-04 10:08:58 +0530

    Create Q53: Write a program to print the following pattern.c

---
+++ Day 27/Q53: Write a program to print the following pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i, j;
+
+    printf("Enter the number of rows for the top half: ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for (i = n - 1; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

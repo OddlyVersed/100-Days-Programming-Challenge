486e8b4d63d071da6896cb3076cedbaf1dba8bf5
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-11-09 17:00:39 +0530

    Add linear search implementation in C
    Implement linear search algorithm to find an element in an array.

---
+++ Day 31/Q61: Search for an element in an array using linear search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int n, i, key, found_index = -1;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            found_index = i;
+            break; 
+        }
+    }
+
+    if (found_index != -1) {
+        printf("Found at index %d\n", found_index);
+    } else {
+        printf("-1\n");
+    }
+
+    return 0;
+}
